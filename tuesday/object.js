fc3ecbebe4f3045334abaf2bf14c99eb932d9ad3
Nishu2696 <nishaanth2696@gmail.com>
2020-05-13 09:35:08 +0530

    Initial commit

---
+++ tuesday/object.js
@@ -0,0 +1,39 @@
+let inputval = "#exmple || @name && user_email";
+inputval=inputval.split(" ");
+console.log(inputval);
+const output=[];
+const outputval= inputval.map((str) => {
+    let obj={};
+    let[first, second] = str.split(/[#@]/);
+    //console.log(first, second);
+    switch(str[0]){
+        case "#":
+            obj.type= "intent";
+            obj.value= second;
+            break;
+
+        case "|":
+            obj.type= 'operator';
+            obj.value= 'OR';
+            break;
+
+        case "@":
+            obj.type= 'entity';
+            obj.value= second;
+            break;
+
+        case "&":
+            obj.type= 'operator';
+            obj.value= 'AND';
+            break;
+
+        default:
+            obj.type= 'variable';
+            obj.value= first;
+            //console.log(first);
+            break;
+    }
+    return obj;
+});
+
+console.log(...output, outputval);
