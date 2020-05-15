447acf375a5f761c0038378af1162a2d95dc5cbf
Nishu2696 <nishaanth2696@gmail.com>
2020-05-15 10:26:01 +0530

    thursday task

--- THURSDAY/musicplayer.js
+++ THURSDAY/musicplayer.js
@@ -6,34 +6,62 @@ var CreateList = /** @class */ (function () {
     }
     return CreateList;
 }());
+var mySongsList = /** @class */ (function () {
+    function mySongsList(config) {
+        this.SongName = config.SongName;
+        this.SongURL = config.SongURL;
+        this.Singer = config.Singer;
+    }
+    return mySongsList;
+}());
 var SongList = /** @class */ (function () {
     function SongList() {
+        this.myLists = [];
     }
     SongList.prototype.addSongs = function (info) {
         this.Songs = info;
+        console.log(this.Songs.SongURL);
+    };
+    SongList.prototype.addedsongs = function (info) {
+        this.myLists.push(info);
     };
     SongList.prototype.mysongs = function () {
-        playSong(this.Songs.SongURL);
+        var name = this.Songs.SongName;
+        console.log(name);
+        var top = document.getElementById("top");
+        var bottom = document.getElementById("songname");
+        document.getElementById("songname").innerHTML = "" + name + "";
+        top.appendChild(bottom);
+        var head = document.getElementById("head");
+        var body = document.getElementById("mysongs");
+        var url = this.Songs.SongURL;
+        console.log(url);
+        document.getElementById("mysongs").setAttribute("src", "" + url + "");
+        document.getElementById("mysongs").setAttribute("type", "audio/mpeg");
+        head.appendChild(body);
+        //addOn(name, url);
     };
     return SongList;
 }());
-function playSong(elements) {
-    var head = document.getElementById("head");
-    var body = document.getElementById("mysongs");
-    //let url=this.Songs.SongURL;
-    console.log(elements);
-    document.getElementById("mysongs").setAttribute("src", elements);
-    document.getElementById("mysongs").setAttribute("type", "audio/mpeg");
-    head.appendChild(body);
-    return false;
-}
+/*function addOn(name1, url1){
+    let li=document.getElementById("lists");
+    for(let i=0;i<this.myLists.length;i++){
+        li.innerHTML += this.myLists[i].toHTML();
+    }
+}*/
 var CreatePlayList = function () {
     var songs = new CreateList({
         SongName: document.getElementById("Songname").value,
-        SongURL: document.getElementById("songurl").value,
+        SongURL: document.getElementById("songurl").files[0].name,
+        Singer: document.getElementById("singer").value
+    });
+    var addsongs = new mySongsList({
+        SongName: document.getElementById("Songname").value,
+        SongURL: document.getElementById("songurl").files[0].name,
         Singer: document.getElementById("singer").value
     });
     var myList = new SongList();
     myList.addSongs(songs);
+    myList.addedsongs(addsongs);
     myList.mysongs();
 };
--- THURSDAY/musicplayer.ts
+++ THURSDAY/musicplayer.ts
@@ -17,36 +17,67 @@ class CreateList{
         this.Singer=config.Singer;
     }
 }
+
+class mySongsList{
+    SongName:Name;
+    SongURL:url;
+    Singer:Artist;
+    constructor(config: SongConfig){
+        this.SongName=config.SongName;
+        this.SongURL=config.SongURL;
+        this.Singer=config.Singer;
+    }
+}
 class SongList{
     Songs:CreateList;
+    myLists:mySongsList[]=[];
     addSongs(info:CreateList){
         this.Songs=info
+        console.log(this.Songs.SongURL);
     }
 
+    addedsongs(info:CreateList){
+        this.myLists.push(info);
+    }
     mysongs(){
-        playSong(this.Songs.SongURL);
+        let name=this.Songs.SongName;
+        console.log(name);
+        let top=document.getElementById("top");
+        let bottom=document.getElementById("songname");
+        document.getElementById("songname").innerHTML=""+name+"";
+        top.appendChild(bottom);
+        let head=document.getElementById("head");
+        let body=document.getElementById("mysongs");
+        let url=this.Songs.SongURL;
+        console.log(url);
+        document.getElementById("mysongs").setAttribute("src", ""+url+"");
+        document.getElementById("mysongs").setAttribute("type", "audio/mpeg");
+        head.appendChild(body);
+        //addOn(name, url);
     }
 }
 
-function playSong(elements){
-    let head=document.getElementById("head");
-    let body=document.getElementById("mysongs");
-    //let url=this.Songs.SongURL;
-    console.log(elements);
-    document.getElementById("mysongs").setAttribute("src", elements);
-    document.getElementById("mysongs").setAttribute("type", "audio/mpeg");
-    head.appendChild(body);
-    return false;
-}
+/*function addOn(name1, url1){
+    let li=document.getElementById("lists");
+    for(let i=0;i<this.myLists.length;i++){
+        li.innerHTML += this.myLists[i].toHTML();
+    }
+}*/
+
 
 let CreatePlayList= () => {
     let songs= new CreateList({
         SongName:(<HTMLInputElement>document.getElementById("Songname")).value as Name,
-        SongURL:(<HTMLInputElement>document.getElementById("songurl")).value as url,
+        SongURL:(<HTMLInputElement>document.getElementById("songurl")).files[0].name as url,
         Singer:(<HTMLInputElement>document.getElementById("singer")).value as Artist
-    })
-
+    });
+    let addsongs= new mySongsList({
+        SongName:(<HTMLInputElement>document.getElementById("Songname")).value as Name,
+        SongURL:(<HTMLInputElement>document.getElementById("songurl")).files[0].name as url,
+        Singer:(<HTMLInputElement>document.getElementById("singer")).value as Artist
+    });
     let myList= new SongList();
     myList.addSongs(songs);
+    myList.addedsongs(addsongs);
     myList.mysongs();
 }
